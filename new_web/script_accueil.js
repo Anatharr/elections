54e278863a1e0d537e5cd42051ccc1141daba8dc
jdhune <jeremie.dhune@insa-cvl.fr>
2021-06-21 22:04:07 +0200

    Finission page d'accueil

--- new_web/script_accueil.js
+++ new_web/script_accueil.js
@@ -1,8 +1,12 @@
+/* DECLARATION VARIABLES */
+
 var menuButton = document.querySelector('#menu-button');
 var menu = document.querySelector('#menu');
 
+/* OUVERTURE ET FERMETURE DU MENU BURGER */
+
 menuButton.addEventListener('click',function(){
     document.getElementById("body").style.background = "images/fr_nb.png"
-    menu.classList.toggle('show-menu');
-    menuButton.classList.toggle('close');
-});
+    menu.classList.toggle('show-menu')
+    menuButton.classList.toggle('close')
+})
