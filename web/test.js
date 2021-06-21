e88988be728e1e585295eae0fac0037d88b7ff31
I4Trazz <jeremie.dhune@gmail.com>
2021-06-21 10:13:34 +0200

    Début de récupération des items dans les listes déroulantes

--- web/test.js
+++ web/test.js
@@ -1,8 +1,6 @@
 let img_dep = document.getElementById("img_dep")
-
-
-
 let img_reg = document.getElementById("img_reg")
+
 let deroulant_dep = document.getElementById("deroulant_dep")
 let label_deroulant_dep = document.getElementById("label_deroulant_dep")
 let deroulant_reg = document.getElementById("deroulant_reg")
@@ -17,19 +15,11 @@ section_departement.style.display = "none"
 section_region.style.display = "none"
 section_france.style.display = "none"
 
+var dep_valeur = document.getElementById("dep_choice").selectedIndex
+document.getElementById("graph_dep").innerHTML = "<span id='test'> Hello </span>"
 
-img_dep.onclick = afficherDep
 
 
-function afficherDep (img_id) {
-    afficherDeroulantDep()
-
-    //var img = document.getElementById(img_id)
-
-    //img.src = '/images/dep_couleur.png'
-
-    //onmouseover
-}
 
 function afficherDeroulantDep () {
     if(getComputedStyle(section_departement).display != "none"){
@@ -58,3 +48,5 @@ function afficherDeroulantFr () {
     section_region.style.display = "none"
     section_france.style.display = "block"
 }
+
+
