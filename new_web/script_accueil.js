58a8bb1b60856801c5323520bff8f32777783ad2
Dorian Beaufils <dorian.beaufils@insa-cvl.fr>
2021-06-23 20:23:10 +0200

    mise en commun

--- new_web/script_accueil.js
+++ new_web/script_accueil.js
@@ -1,12 +1,11 @@
 /* DECLARATION VARIABLES */
 
-var menuButton = document.querySelector('#menu-button');
 var menu = document.querySelector('#menu');
 
 /* OUVERTURE ET FERMETURE DU MENU BURGER */
-
+/*
 menuButton.addEventListener('click',function(){
     document.getElementById("body").style.background = "images/fr_nb.png"
     menu.classList.toggle('show-menu')
-    menuButton.classList.toggle('close')
-})
+  
+})*/
