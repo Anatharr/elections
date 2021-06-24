2a227f4f932a6ff73fc824152b4c71682c60746a
Rochas Nathan <nathan.rochas@insa-cvl.fr>
2021-06-24 16:37:38 +0200

    predictions php

--- new_web/predictions.php
+++ new_web/predictions.php
@@ -18,7 +18,7 @@
     <nav role="navigation">
         <div id="menuToggle">
 
-            <input type="checkbox" id="checkbox_burger" onclick="menu_deroulant_bloquant()"/>
+            <input type="checkbox" id="checkbox_burger" onclick="menu_deroulant_bloquant()" />
 
             <span></span>
             <span></span>
@@ -72,6 +72,7 @@
             <select name="dep_annee" id="dep_annee_choix" onchange="recuperer_departement_annee()">
                 <option value="Default"> --- Année --- </option>
                 <option value="2015">2015</option>
+                <option value="2021">2021</option>
             </select>
         </div>
 
@@ -178,11 +179,9 @@
                 <option value="93">93 - Seine-Saint-Denis</option>
                 <option value="94">94 - Val-de-Marne</option>
                 <option value="95">95 - Val-d'Oise</option>
-                <option value="971">971 - Guadeloupe</option>
-                <option value="972">972 - Martinique</option>
-                <option value="973">973 - Guyane</option>
-                <option value="974">974 - La Réunion</option>
-                <option value="976">976 - Mayotte</option>
+                <option value="ZA">ZA - Guadeloupe</option>
+                <option value="ZD">ZD - La Réunion</option>
+                <option value="ZM">ZM - Mayotte</option>
             </select>
         </div>
 
@@ -248,6 +247,34 @@
             <h2 id="graph_fr">en développement...</h1>
         </div>
     </div>
+
+    <?php
+        // Connexion, sélection de la base de données
+        $dbconn = pg_connect("host=localhost dbname=electionsdb user=pi password=estilections")
+            or die('Connexion impossible : ' . pg_last_error());
+
+        // Exécution de la requête SQL
+        $query = "SELECT (code_canton, nom_canton, nb_inscrits, nb_abstentions, nb_votants, nb_blancs, nb_nuls, nuance_liste_1, sieges_liste_1, nb_voix_liste_1, voix_pourcent_ins_liste_1, nuance_liste_2, sieges_liste_2, nb_voix_liste_2, voix_pourcent_ins_liste_2, nuance_liste_3, sieges_liste_3, nb_voix_liste_3, voix_pourcent_ins_liste_3, nuance_liste_4, sieges_liste_4, nb_voix_liste_4, voix_pourcent_ins_liste_4, nuance_liste_5, sieges_liste_5, nb_voix_liste_5, voix_pourcent_ins_liste_5, nuance_liste_6, sieges_liste_6, nb_voix_liste_6, voix_pourcent_ins_liste_6, nuance_liste_7, sieges_liste_7, nb_voix_liste_7, voix_pourcent_ins_liste_7, nuance_liste_8, sieges_liste_8, nb_voix_liste_8, voix_pourcent_ins_liste_8, nuance_liste_9, sieges_liste_9, nb_voix_liste_9, voix_pourcent_ins_liste_9, nuance_liste_10, sieges_liste_10, nb_voix_liste_10, voix_pourcent_ins_liste_10, nuance_liste_11, sieges_liste_11, nb_voix_liste_11, voix_pourcent_ins_liste_11, nuance_liste_12, sieges_liste_12, nb_voix_liste_12, voix_pourcent_ins_liste_12) FROM public.t1_2015_cantons WHERE (num_departement='18') GROUP BY code_canton";
+        $result = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
+
+        // Affichage des résultats en HTML
+        echo "<table>\n";
+        while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+            echo "\t<tr>\n";
+            foreach ($line as $col_value) {
+                echo "\t\t<td>$col_value</td>\n";
+            }
+            echo "\t</tr>\n";
+        }
+        echo "</table>\n";
+
+        // Libère le résultat
+        pg_free_result($result);
+
+        // Ferme la connexion
+        pg_close($dbconn);
+    ?>
+
 
     <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br>
     <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br> <br><br><br>
