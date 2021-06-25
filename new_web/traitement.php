daae614ed04a66ca5295305d3185a29a31c8c001
Rochas Nathan <nathan.rochas@insa-cvl.fr>
2021-06-25 16:16:39 +0200

    test fix 19

--- new_web/traitement.php
+++ new_web/traitement.php
@@ -6,11 +6,11 @@
         $dbconn = pg_connect("host=localhost dbname=electionsdb user=pi password=estilections")
         or die('Connexion impossible : ' . pg_last_error());
 
-        $table = "public.t1_".$annee."_cantons";
+        $table = "t1_".$annee."_cantons";
         echo $table;
          
         // Exécution de la requête SQL
-        $query = "SELECT code_canton, nom_canton, nuance_liste_1, voix_pourcent_ins_liste_1, nuance_liste_2, voix_pourcent_ins_liste_2, nuance_liste_3, voix_pourcent_ins_liste_3, nuance_liste_4, voix_pourcent_ins_liste_4, nuance_liste_5, voix_pourcent_ins_liste_5, nuance_liste_6, voix_pourcent_ins_liste_6, nuance_liste_7, voix_pourcent_ins_liste_7, nuance_liste_8, voix_pourcent_ins_liste_8, nuance_liste_9, voix_pourcent_ins_liste_9, nuance_liste_10, voix_pourcent_ins_liste_10, nuance_liste_11, voix_pourcent_ins_liste_11, nuance_liste_12, voix_pourcent_ins_liste_12 FROM $1 WHERE num_departement=$2;";
+        $query = "SELECT code_canton, nom_canton, nuance_liste_1, voix_pourcent_ins_liste_1, nuance_liste_2, voix_pourcent_ins_liste_2, nuance_liste_3, voix_pourcent_ins_liste_3, nuance_liste_4, voix_pourcent_ins_liste_4, nuance_liste_5, voix_pourcent_ins_liste_5, nuance_liste_6, voix_pourcent_ins_liste_6, nuance_liste_7, voix_pourcent_ins_liste_7, nuance_liste_8, voix_pourcent_ins_liste_8, nuance_liste_9, voix_pourcent_ins_liste_9, nuance_liste_10, voix_pourcent_ins_liste_10, nuance_liste_11, voix_pourcent_ins_liste_11, nuance_liste_12, voix_pourcent_ins_liste_12 FROM public.$1 WHERE num_departement=$2;";
         $result = pg_query_params($dbconn, $query, array($table, $num_departement)) or die('Échec de la requête : ' . pg_last_error());
 
         // Ferme la connexion
