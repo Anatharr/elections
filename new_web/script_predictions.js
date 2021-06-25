3a602fd5ee77916516e09ba15c0364ee05e8bb71
Rochas Nathan <nathan.rochas@insa-cvl.fr>
2021-06-25 18:51:02 +0200

    test fix 35

--- new_web/script_predictions.js
+++ new_web/script_predictions.js
@@ -152,9 +152,10 @@ function lancer_recherche_dep() {
             }
             console.log("ici");
             document.getElementById("tour_1").innerHTML = "<table>";
-            for (i = 0; i <= resultat_php.length; i++) {
+            for (i = 0; i < resultat_php.length; i++) {
                 document.getElementById("tour_1").innerHTML = "<tr>";
-                for (j = 0; j <= resultat_php[i].length; j++) {
+                console.log(type(resultat_php[i]));
+                for (j = 0; j < resultat_php[i].length; j++) {
                     document.getElementById("tour_1").innerHTML = "<td>";
                     document.getElementById("tour_1").innerHTML = resultat_php[i][j];
                     document.getElementById("tour_1").innerHTML = "</td>";
