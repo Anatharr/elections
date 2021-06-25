a86a189023e66e2fd4935dd325a88f37bb2d1fe9
Rochas Nathan <nathan.rochas@insa-cvl.fr>
2021-06-25 17:08:01 +0200

    test fix 21

--- new_web/traitement.php
+++ new_web/traitement.php
@@ -2,13 +2,49 @@
 
     function affiche_tour_1_departement($annee, $num_departement) {
 
+        $result = array();
+
         // Connexion, sélection de la base de données
         $dbconn = pg_connect("host=localhost dbname=electionsdb user=pi password=estilections")
         or die('Connexion impossible : ' . pg_last_error());
-         
-        // Exécution de la requête SQL
-        $query = "SELECT code_canton, nom_canton, nuance_liste_1, voix_pourcent_ins_liste_1, nuance_liste_2, voix_pourcent_ins_liste_2, nuance_liste_3, voix_pourcent_ins_liste_3, nuance_liste_4, voix_pourcent_ins_liste_4, nuance_liste_5, voix_pourcent_ins_liste_5, nuance_liste_6, voix_pourcent_ins_liste_6, nuance_liste_7, voix_pourcent_ins_liste_7, nuance_liste_8, voix_pourcent_ins_liste_8, nuance_liste_9, voix_pourcent_ins_liste_9, nuance_liste_10, voix_pourcent_ins_liste_10, nuance_liste_11, voix_pourcent_ins_liste_11, nuance_liste_12, voix_pourcent_ins_liste_12 FROM public.t1_(\$1)_cantons WHERE num_departement=$2;";
-        $result = pg_query_params($dbconn, $query, array($table, $num_departement)) or die('Échec de la requête : ' . pg_last_error());
+        switch ($annee) {
+
+            case 2008:
+
+                // Exécution de la requête SQL sécurisée
+                $query = "SELECT code_canton, nom_canton, nuance_liste_1, voix_pourcent_ins_liste_1, nuance_liste_2, voix_pourcent_ins_liste_2, nuance_liste_3, voix_pourcent_ins_liste_3, nuance_liste_4, voix_pourcent_ins_liste_4, nuance_liste_5, voix_pourcent_ins_liste_5, nuance_liste_6, voix_pourcent_ins_liste_6, nuance_liste_7, voix_pourcent_ins_liste_7, nuance_liste_8, voix_pourcent_ins_liste_8, nuance_liste_9, voix_pourcent_ins_liste_9, nuance_liste_10, voix_pourcent_ins_liste_10, nuance_liste_11, voix_pourcent_ins_liste_11, nuance_liste_12, voix_pourcent_ins_liste_12 FROM public.t1_2008_cantons WHERE num_departement=$1;";
+                $result['result'] = pg_query_params($dbconn, $query, array($num_departement)) or die('Échec de la requête : ' . pg_last_error());
+                break;
+
+            case 2011:
+
+                // Exécution de la requête SQL sécurisée
+                $query = "SELECT code_canton, nom_canton, nuance_liste_1, voix_pourcent_ins_liste_1, nuance_liste_2, voix_pourcent_ins_liste_2, nuance_liste_3, voix_pourcent_ins_liste_3, nuance_liste_4, voix_pourcent_ins_liste_4, nuance_liste_5, voix_pourcent_ins_liste_5, nuance_liste_6, voix_pourcent_ins_liste_6, nuance_liste_7, voix_pourcent_ins_liste_7, nuance_liste_8, voix_pourcent_ins_liste_8, nuance_liste_9, voix_pourcent_ins_liste_9, nuance_liste_10, voix_pourcent_ins_liste_10, nuance_liste_11, voix_pourcent_ins_liste_11, nuance_liste_12, voix_pourcent_ins_liste_12 FROM public.t1_2011_cantons WHERE num_departement=$1;";
+                $result['result'] = pg_query_params($dbconn, $query, array($num_departement)) or die('Échec de la requête : ' . pg_last_error());
+                break;
+
+            case 2015:
+
+                // Exécution de la requête SQL sécurisée
+                $query = "SELECT code_canton, nom_canton, nuance_liste_1, voix_pourcent_ins_liste_1, nuance_liste_2, voix_pourcent_ins_liste_2, nuance_liste_3, voix_pourcent_ins_liste_3, nuance_liste_4, voix_pourcent_ins_liste_4, nuance_liste_5, voix_pourcent_ins_liste_5, nuance_liste_6, voix_pourcent_ins_liste_6, nuance_liste_7, voix_pourcent_ins_liste_7, nuance_liste_8, voix_pourcent_ins_liste_8, nuance_liste_9, voix_pourcent_ins_liste_9, nuance_liste_10, voix_pourcent_ins_liste_10, nuance_liste_11, voix_pourcent_ins_liste_11, nuance_liste_12, voix_pourcent_ins_liste_12 FROM public.t1_2015_cantons WHERE num_departement=$1;";
+                $result['result'] = pg_query_params($dbconn, $query, array($num_departement)) or die('Échec de la requête : ' . pg_last_error());
+                break;
+
+            case 2021:
+
+                // Exécution de la requête SQL sécurisée
+                $query = "SELECT code_canton, nom_canton, nuance_liste_1, voix_pourcent_ins_liste_1, nuance_liste_2, voix_pourcent_ins_liste_2, nuance_liste_3, voix_pourcent_ins_liste_3, nuance_liste_4, voix_pourcent_ins_liste_4, nuance_liste_5, voix_pourcent_ins_liste_5, nuance_liste_6, voix_pourcent_ins_liste_6, nuance_liste_7, voix_pourcent_ins_liste_7, nuance_liste_8, voix_pourcent_ins_liste_8, nuance_liste_9, voix_pourcent_ins_liste_9, nuance_liste_10, voix_pourcent_ins_liste_10, nuance_liste_11, voix_pourcent_ins_liste_11, nuance_liste_12, voix_pourcent_ins_liste_12 FROM public.t1_2021_cantons WHERE num_departement=$1;";
+                $result['result'] = pg_query_params($dbconn, $query, array($num_departement)) or die('Échec de la requête : ' . pg_last_error());
+                break;
+
+            default:
+                
+                //Si l'année n'est pas valide
+                $result['error'] = "Pas d'information pour l'année ".$annee.;
+                break;
+            
+        }
+
 
         // Ferme la connexion
         pg_close($dbconn);
@@ -35,7 +71,7 @@
                     $resultat_requete['error'] = "Erreur d'argument !";
                 }
                 else {
-                    $resultat_requete['result'] = affiche_tour_1_departement( strval($_POST['arguments'][0]), strval($_POST['arguments'][1]) );
+                    $resultat_requete['result'] = affiche_tour_1_departement( intval($_POST['arguments'][0]), strval($_POST['arguments'][1]) );
                 }
                 break;
             
@@ -82,6 +118,10 @@
                 else {
                     $resultat_requete['result'] = estimation_tour_2_region( intval($_POST['arguments'][0]), strval($_POST['arguments'][1]) );
                 }
+                break;
+
+            default:
+                $resultat_requete['error'] = "La fonction ".$_POST['functionname']." n'a pas été trouvée";
                 break;
         }
     }
