cd1cdd23ba42a0d2a2c97d9293516df4e216e240
Rochas Nathan <nathan.rochas@insa-cvl.fr>
2021-06-25 15:02:15 +0200

    remove php

--- new_web/predictions.php
+++ new_web/predictions.php
@@ -260,39 +260,6 @@
             <h2 id="graph_fr">en développement...</h1>
         </div>
     </div>
-
-    <div id="container_php">
-        <?php
-            // Connexion, sélection de la base de données
-            $dbconn = pg_connect("host=localhost dbname=electionsdb user=pi password=estilections")
-                or die('Connexion impossible : ' . pg_last_error());
-
-            $annee = $_POST["dept_annee"];
-                
-            // Exécution de la requête SQL
-            $query = "SELECT code_canton, nom_canton, nb_inscrits, nuance_liste_1, voix_pourcent_ins_liste_1, nuance_liste_2, voix_pourcent_ins_liste_2, nuance_liste_3, voix_pourcent_ins_liste_3, nuance_liste_4, voix_pourcent_ins_liste_4, nuance_liste_5, voix_pourcent_ins_liste_5, nuance_liste_6, voix_pourcent_ins_liste_6, nuance_liste_7, voix_pourcent_ins_liste_7, nuance_liste_8, voix_pourcent_ins_liste_8, nuance_liste_9, voix_pourcent_ins_liste_9, nuance_liste_10, voix_pourcent_ins_liste_10, nuance_liste_11, voix_pourcent_ins_liste_11, nuance_liste_12, voix_pourcent_ins_liste_12 FROM public.t1_2015_cantons WHERE num_departement='18';";
-            $result = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
-
-            // Affichage des résultats en HTML
-            
-            echo "<table id='tour1_tab'>\n";
-            while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-                echo "\t<tr class='tour1_ligne'>\n";
-                foreach ($line as $col_value) {
-                    echo "\t\t<td class='tour1_colonne'>$col_value</td>\n";
-                }
-                echo "\t</tr>\n";
-            }
-            echo "</table>\n";
-
-            // Libère le résultat
-            pg_free_result($result);
-
-            // Ferme la connexion
-            pg_close($dbconn);
-        ?>
-    </div>
-
     
     <script src="script_predictions.js"></script>
     <script src="script_accueil.js"></script>
