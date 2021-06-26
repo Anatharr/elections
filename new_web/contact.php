7b3353139f15e7f6ef8b83584acd6a2559790d25
Rochas Nathan <nathan.rochas@insa-cvl.fr>
2021-06-27 01:53:13 +0200

    fix contact 1

--- new_web/contact.php
+++ new_web/contact.php
@@ -59,18 +59,18 @@
                 <input type="submit">
             </form>
             <?php
-            if (isset($_POST['message'])) {
-                $position_arobase = strpos($_POST['email'], '@');
-                if ($position_arobase === false)
-                    echo '<p>Votre email doit comporter un arobase.</p>';
-                else {
-                    $retour = mail('jules@free.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: ' . $_POST['email']);
-                    if($retour)
-                        echo '<p>Votre message a été envoyé.</p>';
-                    else
-                        echo '<p>Erreur.</p>';
+                if (isset($_POST['message'])) {
+                    $position_arobase = strpos($_POST['email'], '@');
+                    if ($position_arobase === false)
+                        echo '<p>Votre email doit comporter un arobase.</p>';
+                    else {
+                        $retour = mail('nathan.rochas@insa-cvl.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: ' . $_POST['email']);
+                        if($retour)
+                            echo '<p>Votre message a été envoyé.</p>';
+                        else
+                            echo '<p>Erreur</p>';
+                    }
                 }
-            }
             ?>
                 <br>
                 <br>
