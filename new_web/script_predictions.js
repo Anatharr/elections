3a3005bd210a6eb1bbca547ebe79b9e014ecd70d
Dorian Beaufils <dorian.beaufils@insa-cvl.fr>
2021-06-26 23:54:34 +0200

    bug

--- new_web/script_predictions.js
+++ new_web/script_predictions.js
@@ -147,7 +147,7 @@ function lancer_recherche_dep() {
         dataType: 'json',
         data: { functionname: 'affiche_tour_1_departement', arguments: [document.getElementById("dep_annee_choix").options[deroulant_dep_annee.selectedIndex].value, document.getElementById("dep_choix").options[deroulant_dep.selectedIndex].value] },
 
-        success: function(obj, textstatus) {
+        success: function (obj, textstatus) {
             if (!('error' in obj)) {
                 resultat_php = obj.result;
             } else {
@@ -229,19 +229,16 @@ function lancer_recherche_dep() {
 
             obj_dept.cantons = new Array(nombre_canton);
 
-
-            for (i = 0; i < resultat_php.length; i++) {
-
-
-                if (document.getElementById("dep_annee_choix").options[deroulant_dep_annee.selectedIndex].value == 2015) {
-                    var nombre_nuances = 19;
-                    for (k = 0; k < nombre_canton; k++) {
+            if (document.getElementById("dep_annee_choix").options[deroulant_dep_annee.selectedIndex].value == 2015) {
+                var nombre_nuances = 19;
+                for (k = 0; k < nombre_canton; k++) {
 
 
-                        obj_dept.cantons[k] = { name: resultat_php[k][1], parti: new Array(nombre_nuances) };
+                    obj_dept.cantons[k] = { name: resultat_php[k][1], parti: new Array(nombre_nuances) };
 
-                    }
+                }
 
+                for (i = 0; i < resultat_php.length; i++) {
 
                     obj_dept.cantons[i].parti[0] = { name: 'BC-EXG', score: 0 };
                     obj_dept.cantons[i].parti[1] = { name: 'BC-FG', score: 0 };
@@ -263,25 +260,23 @@ function lancer_recherche_dep() {
                     obj_dept.cantons[i].parti[17] = { name: 'BC-FN', score: 0 };
                     obj_dept.cantons[i].parti[18] = { name: 'BC-EXD', score: 0 };
 
-                    console.table(obj_dept.cantons[i].parti[0]);
-                }
+                
 
-
-
-                for (j = 3; j < resultat_php[i].length; j += 2) {
-                    if (resultat_php[i][j] != null) {
-                        for (k = 0; k < nombre_nuances; k++) {
-                            if (obj_dept.cantons[i].parti[k].name == resultat_php[i][j - 1]) {
-                                obj_dept.cantons[i].parti[k].score = resultat_php[i][j];
+                    for (j = 3; j < resultat_php[i].length; j += 2) {
+                        if (resultat_php[i][j] != null) {
+                            for (k = 0; k < nombre_nuances; k++) {
+                                if (obj_dept.cantons[i].parti[k].name == resultat_php[i][j - 1]) {
+                                    obj_dept.cantons[i].parti[k].score = resultat_php[i][j];
+                                }
                             }
-                        }
 
+                        }
                     }
                 }
 
-
             }
 
+            console.log(obj_dept);
             /*** Création tab contenant les data de chaque partie par canton */
             var tab = [];
             for (j = 0; j < nombre_nuances; j++) {
@@ -295,100 +290,100 @@ function lancer_recherche_dep() {
             const data = {
                 labels: labels,
                 datasets: [{
-                        label: 'BC-EXG',
-                        data: tab[0],
-                        backgroundColor: 'rgb(187, 0, 0)',
-                    },
-                    {
-                        label: 'BC-FG',
-                        data: tab[1],
-                        backgroundColor: 'rgb(221, 0, 0)',
-                    },
-                    {
-                        label: 'BC-COM',
-                        data: tab[2],
-                        backgroundColor: 'rgb(221, 0, 0)',
-                    },
-                    {
-                        label: 'BC-PG',
-                        data: tab[3],
-                        backgroundColor: 'rgb(245,142,187)',
-                    },
-                    {
-                        label: 'BC-RDG',
-                        data: tab[4],
-                        backgroundColor: 'rgb(255, 209, 220)',
-                    },
-                    {
-                        label: 'BC-UG',
-                        data: tab[5],
-                        backgroundColor: 'rgb(204, 102, 102)',
-                    },
-                    {
-                        label: 'BC-VEC',
-                        data: tab[6],
-                        backgroundColor: 'rgb(0, 192, 0)',
-                    },
-                    {
-                        label: 'BC-DVG',
-                        data: tab[7],
-                        backgroundColor: 'rgb(255, 192, 192)',
-                    },
-                    {
-                        label: 'BC-SOC',
-                        data: tab[8],
-                        backgroundColor: 'rgb(255, 128, 128)',
-                    },
-                    {
-                        label: 'BC-MDM',
-                        data: tab[9],
-                        backgroundColor: 'rgb(255, 153, 0)',
-                    },
-                    {
-                        label: 'BC-UC',
-                        data: tab[10],
-                        backgroundColor: 'rgb(255, 235, 0)',
-                    },
-                    {
-                        label: 'BC-DIV',
-                        data: tab[11],
-                        backgroundColor: 'rgb(238, 238, 238)',
-                    },
-                    {
-                        label: 'BC-UD',
-                        data: tab[12],
-                        backgroundColor: 'rgb(135, 206, 250)',
-                    },
-                    {
-                        label: 'BC-UDI',
-                        data: tab[13],
-                        backgroundColor: 'rgb(0, 255, 255)',
-                    },
-                    {
-                        label: 'BC-DVD',
-                        data: tab[14],
-                        backgroundColor: 'rgb(173, 193, 253)',
-                    },
-                    {
-                        label: 'BC-LR',
-                        data: tab[15],
-                        backgroundColor: 'rgb(0, 102, 204)',
-                    },
-                    {
-                        label: 'BC-DLF',
-                        data: tab[16],
-                        backgroundColor: 'rgb(0, 130, 196)',
-                    },
-                    {
-                        label: 'BC-RN',
-                        data: tab[17],
-                        backgroundColor: 'rgb(13, 55, 138)',
-                    },
-                    {
-                        label: 'BC-EXD',
-                        data: tab[18],
-                        backgroundColor: 'rgb(64, 64, 64)',
-                    },
+                    label: 'BC-EXG',
+                    data: tab[0],
+                    backgroundColor: 'rgb(187, 0, 0)',
+                },
+                {
+                    label: 'BC-FG',
+                    data: tab[1],
+                    backgroundColor: 'rgb(221, 0, 0)',
+                },
+                {
+                    label: 'BC-COM',
+                    data: tab[2],
+                    backgroundColor: 'rgb(221, 0, 0)',
+                },
+                {
+                    label: 'BC-PG',
+                    data: tab[3],
+                    backgroundColor: 'rgb(245,142,187)',
+                },
+                {
+                    label: 'BC-RDG',
+                    data: tab[4],
+                    backgroundColor: 'rgb(255, 209, 220)',
+                },
+                {
+                    label: 'BC-UG',
+                    data: tab[5],
+                    backgroundColor: 'rgb(204, 102, 102)',
+                },
+                {
+                    label: 'BC-VEC',
+                    data: tab[6],
+                    backgroundColor: 'rgb(0, 192, 0)',
+                },
+                {
+                    label: 'BC-DVG',
+                    data: tab[7],
+                    backgroundColor: 'rgb(255, 192, 192)',
+                },
+                {
+                    label: 'BC-SOC',
+                    data: tab[8],
+                    backgroundColor: 'rgb(255, 128, 128)',
+                },
+                {
+                    label: 'BC-MDM',
+                    data: tab[9],
+                    backgroundColor: 'rgb(255, 153, 0)',
+                },
+                {
+                    label: 'BC-UC',
+                    data: tab[10],
+                    backgroundColor: 'rgb(255, 235, 0)',
+                },
+                {
+                    label: 'BC-DIV',
+                    data: tab[11],
+                    backgroundColor: 'rgb(238, 238, 238)',
+                },
+                {
+                    label: 'BC-UD',
+                    data: tab[12],
+                    backgroundColor: 'rgb(135, 206, 250)',
+                },
+                {
+                    label: 'BC-UDI',
+                    data: tab[13],
+                    backgroundColor: 'rgb(0, 255, 255)',
+                },
+                {
+                    label: 'BC-DVD',
+                    data: tab[14],
+                    backgroundColor: 'rgb(173, 193, 253)',
+                },
+                {
+                    label: 'BC-LR',
+                    data: tab[15],
+                    backgroundColor: 'rgb(0, 102, 204)',
+                },
+                {
+                    label: 'BC-DLF',
+                    data: tab[16],
+                    backgroundColor: 'rgb(0, 130, 196)',
+                },
+                {
+                    label: 'BC-RN',
+                    data: tab[17],
+                    backgroundColor: 'rgb(13, 55, 138)',
+                },
+                {
+                    label: 'BC-EXD',
+                    data: tab[18],
+                    backgroundColor: 'rgb(64, 64, 64)',
+                },
 
                 ]
 
@@ -421,7 +416,7 @@ function lancer_recherche_dep() {
 
         },
 
-        error: function(chr, ajaxOptions, thrownError) {
+        error: function (chr, ajaxOptions, thrownError) {
             alert(chr.responseText); //Ce code affichera le message d'erreur, ici Message d'erreur.
         }
 
@@ -439,7 +434,7 @@ function lancer_recherche_dep() {
         dataType: 'json',
         data: { functionname: 'affiche_tour_2_departement', arguments: [document.getElementById("dep_annee_choix").options[deroulant_dep_annee.selectedIndex].value, document.getElementById("dep_choix").options[deroulant_dep.selectedIndex].value] },
 
-        success: function(obj, textstatus) {
+        success: function (obj, textstatus) {
             if (!('error' in obj)) {
                 resultat_php = obj.result;
             } else {
@@ -472,7 +467,7 @@ function lancer_recherche_dep() {
             document.getElementById("tour_2").innerHTML = chaine;
 
             /* Traitement des données du tableau, création d'un dictionnaire regroupant les cantons et les scores par partis */
-            var obj_dept = new Object();
+            /*var obj_dept = new Object();
             let nombre_canton = resultat_php.length;
             obj_dept.name = document.getElementById("dep_choix").options[deroulant_dep.selectedIndex].value;
 
@@ -538,100 +533,100 @@ function lancer_recherche_dep() {
             const data = {
                 labels: labels,
                 datasets: [{
-                        label: 'BC-EXG',
-                        data: tab[0],
-                        backgroundColor: 'rgb(187, 0, 0)',
-                    },
-                    {
-                        label: 'BC-FG',
-                        data: tab[1],
-                        backgroundColor: 'rgb(221, 0, 0)',
-                    },
-                    {
-                        label: 'BC-COM',
-                        data: tab[2],
-                        backgroundColor: 'rgb(221, 0, 0)',
-                    },
-                    {
-                        label: 'BC-PG',
-                        data: tab[3],
-                        backgroundColor: 'rgb(245,142,187)',
-                    },
-                    {
-                        label: 'BC-RDG',
-                        data: tab[4],
-                        backgroundColor: 'rgb(255, 209, 220)',
-                    },
-                    {
-                        label: 'BC-UG',
-                        data: tab[5],
-                        backgroundColor: 'rgb(204, 102, 102)',
-                    },
-                    {
-                        label: 'BC-VEC',
-                        data: tab[6],
-                        backgroundColor: 'rgb(0, 192, 0)',
-                    },
-                    {
-                        label: 'BC-DVG',
-                        data: tab[7],
-                        backgroundColor: 'rgb(255, 192, 192)',
-                    },
-                    {
-                        label: 'BC-SOC',
-                        data: tab[8],
-                        backgroundColor: 'rgb(255, 128, 128)',
-                    },
-                    {
-                        label: 'BC-MDM',
-                        data: tab[9],
-                        backgroundColor: 'rgb(255, 153, 0)',
-                    },
-                    {
-                        label: 'BC-UC',
-                        data: tab[10],
-                        backgroundColor: 'rgb(255, 235, 0)',
-                    },
-                    {
-                        label: 'BC-DIV',
-                        data: tab[11],
-                        backgroundColor: 'rgb(238, 238, 238)',
-                    },
-                    {
-                        label: 'BC-UD',
-                        data: tab[12],
-                        backgroundColor: 'rgb(135, 206, 250)',
-                    },
-                    {
-                        label: 'BC-UDI',
-                        data: tab[13],
-                        backgroundColor: 'rgb(0, 255, 255)',
-                    },
-                    {
-                        label: 'BC-DVD',
-                        data: tab[14],
-                        backgroundColor: 'rgb(173, 193, 253)',
-                    },
-                    {
-                        label: 'BC-LR',
-                        data: tab[15],
-                        backgroundColor: 'rgb(0, 102, 204)',
-                    },
-                    {
-                        label: 'BC-DLF',
-                        data: tab[16],
-                        backgroundColor: 'rgb(0, 130, 196)',
-                    },
-                    {
-                        label: 'BC-RN',
-                        data: tab[17],
-                        backgroundColor: 'rgb(13, 55, 138)',
-                    },
-                    {
-                        label: 'BC-EXD',
-                        data: tab[18],
-                        backgroundColor: 'rgb(64, 64, 64)',
-                    },
+                    label: 'BC-EXG',
+                    data: tab[0],
+                    backgroundColor: 'rgb(187, 0, 0)',
+                },
+                {
+                    label: 'BC-FG',
+                    data: tab[1],
+                    backgroundColor: 'rgb(221, 0, 0)',
+                },
+                {
+                    label: 'BC-COM',
+                    data: tab[2],
+                    backgroundColor: 'rgb(221, 0, 0)',
+                },
+                {
+                    label: 'BC-PG',
+                    data: tab[3],
+                    backgroundColor: 'rgb(245,142,187)',
+                },
+                {
+                    label: 'BC-RDG',
+                    data: tab[4],
+                    backgroundColor: 'rgb(255, 209, 220)',
+                },
+                {
+                    label: 'BC-UG',
+                    data: tab[5],
+                    backgroundColor: 'rgb(204, 102, 102)',
+                },
+                {
+                    label: 'BC-VEC',
+                    data: tab[6],
+                    backgroundColor: 'rgb(0, 192, 0)',
+                },
+                {
+                    label: 'BC-DVG',
+                    data: tab[7],
+                    backgroundColor: 'rgb(255, 192, 192)',
+                },
+                {
+                    label: 'BC-SOC',
+                    data: tab[8],
+                    backgroundColor: 'rgb(255, 128, 128)',
+                },
+                {
+                    label: 'BC-MDM',
+                    data: tab[9],
+                    backgroundColor: 'rgb(255, 153, 0)',
+                },
+                {
+                    label: 'BC-UC',
+                    data: tab[10],
+                    backgroundColor: 'rgb(255, 235, 0)',
+                },
+                {
+                    label: 'BC-DIV',
+                    data: tab[11],
+                    backgroundColor: 'rgb(238, 238, 238)',
+                },
+                {
+                    label: 'BC-UD',
+                    data: tab[12],
+                    backgroundColor: 'rgb(135, 206, 250)',
+                },
+                {
+                    label: 'BC-UDI',
+                    data: tab[13],
+                    backgroundColor: 'rgb(0, 255, 255)',
+                },
+                {
+                    label: 'BC-DVD',
+                    data: tab[14],
+                    backgroundColor: 'rgb(173, 193, 253)',
+                },
+                {
+                    label: 'BC-LR',
+                    data: tab[15],
+                    backgroundColor: 'rgb(0, 102, 204)',
+                },
+                {
+                    label: 'BC-DLF',
+                    data: tab[16],
+                    backgroundColor: 'rgb(0, 130, 196)',
+                },
+                {
+                    label: 'BC-RN',
+                    data: tab[17],
+                    backgroundColor: 'rgb(13, 55, 138)',
+                },
+                {
+                    label: 'BC-EXD',
+                    data: tab[18],
+                    backgroundColor: 'rgb(64, 64, 64)',
+                },
 
                 ]
 
@@ -660,11 +655,11 @@ function lancer_recherche_dep() {
 
             var chainegraph = '<canvas id="myChart" width="1000" height="350"></canvas>';
             document.getElementById("graphe").innerHTML = chainegraph;
-            var myChart = new Chart(document.getElementById('myChart'), config);
+            var myChart = new Chart(document.getElementById('myChart'), config);*/
 
         },
 
-        error: function(chr, ajaxOptions, thrownError) {
+        error: function (chr, ajaxOptions, thrownError) {
             alert(chr.responseText); //Ce code affichera le message d'erreur, ici Message d'erreur.
         }
 
