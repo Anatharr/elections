e483d198a2cf60b6e32d4cdbb9388e2eb4bc4bd1
Cyprien Leschi <cyprien.leschi@gmail.com>
2021-06-28 05:51:09 +0200

    ia

--- new_web/ia.js
+++ new_web/ia.js
@@ -4,6 +4,8 @@ available_models = ['BC-COM_BC-DVD_BC-FN', 'BC-COM_BC-FN', 'BC-COM_BC-FN_BC-UD',
     'BC-DVD_BC-DVG', 'BC-DVD_BC-DVG_BC-FN', 'BC-DVD_BC-FG', 'BC-DVD_BC-FN', 'BC-DVD_BC-FN_BC-RDG', 'BC-DVD_BC-FN_BC-SOC', 'BC-DVD_BC-FN_BC-UD', 'BC-DVD_BC-FN_BC-UDI', 'BC-DVD_BC-FN_BC-UG', 'BC-DVD_BC-FN_BC-UMP', 'BC-DVD_BC-RDG', 'BC-DVD_BC-SOC', 'BC-DVD_BC-UD', 'BC-DVD_BC-UDI', 'BC-DVD_BC-UDI_BC-UG', 'BC-DVD_BC-UG', 'BC-DVD_BC-UMP', 'BC-DVD_BC-VEC', 'BC-DVG_BC-FG', 'BC-DVG_BC-FN', 'BC-DVG_BC-FN_BC-RDG', 'BC-DVG_BC-FN_BC-SOC', 'BC-DVG_BC-FN_BC-UD', 'BC-DVG_BC-FN_BC-UDI', 'BC-DVG_BC-FN_BC-UMP', 'BC-DVG_BC-MDM', 'BC-DVG_BC-SOC', 'BC-DVG_BC-UC', 'BC-DVG_BC-UD', 'BC-DVG_BC-UDI', 'BC-DVG_BC-UG', 'BC-DVG_BC-UMP', 'BC-EXD_BC-FN', 'BC-EXD_BC-UG', 'BC-EXD_BC-UMP', 'BC-FG_BC-FN', 'BC-FG_BC-FN_BC-UD', 'BC-FG_BC-SOC', 'BC-FG_BC-UD', 'BC-FG_BC-UDI', 'BC-FG_BC-UG', 'BC-FG_BC-UMP', 'BC-FN_BC-MDM', 'BC-FN_BC-MDM_BC-UD', 'BC-FN_BC-RDG', 'BC-FN_BC-RDG_BC-UD', 'BC-FN_BC-SOC', 'BC-FN_BC-SOC_BC-UC', 'BC-FN_BC-SOC_BC-UD', 'BC-FN_BC-SOC_BC-UDI', 'BC-FN_BC-SOC_BC-UMP', 'BC-FN_BC-UD', 'BC-FN_BC-UDI', 'BC-FN_BC-UDI_BC-UG', 'BC-FN_BC-UD_BC-UG', 'BC-FN_BC-UG', 'BC-FN_BC-UG_BC-UMP', 'BC-FN_BC-UMP', 'BC-FN_BC-UMP_BC-VEC', 'BC-FN_BC-VEC', 'BC-MDM_BC-SOC', 'BC-MDM_BC-UMP', 'BC-RDG_BC-SOC', 'BC-RDG_BC-UD', 'BC-RDG_BC-UDI', 'BC-RDG_BC-UG', 'BC-SOC_BC-UC', 'BC-SOC_BC-UD', 'BC-SOC_BC-UDI', 'BC-SOC_BC-UMP', 'BC-UC_BC-UG', 'BC-UDI_BC-UG', 'BC-UDI_BC-UMP', 'BC-UD_BC-UDI', 'BC-UD_BC-UG', 'BC-UD_BC-UMP', 'BC-UD_BC-VEC', 'BC-UG_BC-UMP', 'BC-UMP_BC-VEC'
 ]
 
+var dataT1 = null;
+
 $(document).ready(function() {
     sel = document.getElementById('model_year')
     available_models.forEach(e => {
@@ -72,7 +74,7 @@ async function load_model(year, canton) {
 
       success: function(obj, textstatus) {
           if (!('error' in obj)) {
-              const dataT1 = obj.result
+              dataT1 = obj.result
           } else {
               console.log(obj.error);
           }
