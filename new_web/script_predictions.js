cce84035a1509fc327e6b45aae1ee660a8e98020
I4Trazz <jeremie.dhune@gmail.com>
2021-06-28 05:05:29 +0200

    dev

--- new_web/script_predictions.js
+++ new_web/script_predictions.js
@@ -810,10 +810,31 @@ function recupererCsv (departement, canton) {
     url: "/datasets/XDataFR_2015_Can.csv",
     dataType: "text",
     success: function(data) {
+
+        var allTextLines = allText.split(/\r\n|\n/);
+        var headers = allTextLines[0].split(',');
+        var lines = [];
+
+        for (var i=1; i<allTextLines.length; i++) {
+            var data = allTextLines[i].split(',');
+            if (data.length == headers.length) {
+
+                var tarr = [];
+                for (var j=0; j<headers.length; j++) {
+                    tarr.push(headers[j]+":"+data[j]);
+                }
+                lines.push(tarr);
+            }
+        }
+        console.log(lines);
+    
+
+
+
         let ligne;
         let j = 0;
         let tab_final = [];
-        console.log(data);
+        //console.log(data);
         for (let i = 0; i < data.length; i++) {
             if (data[i][0] == departement) {
                 ligne = i;
@@ -833,7 +854,7 @@ function recupererCsv (departement, canton) {
             }
             j++;
         }
-        console.log(tab_final);
+        //console.log(tab_final);
         return tab_final;
     }
     });
