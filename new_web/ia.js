8fd73c2617f8b58f61968eeb780775738c188b3c
Cyprien Leschi <cyprien.leschi@gmail.com>
2021-06-28 06:40:14 +0200

    efjs

--- new_web/ia.js
+++ new_web/ia.js
@@ -84,14 +84,15 @@ async function load_model(dataT1, year, canton) {
 
 
 async function lancer_prediction() {
-    const yearSelect = document.getElementById('model_name')
+    const modelSelect = document.getElementById('model_name')
     const dptSelect = document.getElementById("dep_annee_choix")
     const cantonSelect = document.getElementById('model_canton')
-    const year = yearSelect.options[yearSelect.selectedIndex].value
+    const modelname = modelSelect.options[modelSelect.selectedIndex].value
     const dpt = dptSelect.options[dptSelect.selectedIndex].value
     const canton = cantonSelect.options[cantonSelect.selectedIndex].value
     let a = null;
     let b = canton;
+    const year=2019;
 
     if (canton=='default') {
       alert("Veuillez spécifier un canton pour réaliser la prédiction.");
